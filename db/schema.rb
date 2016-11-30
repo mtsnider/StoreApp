d2d51b444c8ef3f6d2650439af40713802378dd7
melisserrr <mtsnider@gmail.com>
2016-11-29 20:12:32 -0600

    forgot to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130012936) do
+ActiveRecord::Schema.define(version: 20161130020132) do
 
   create_table "aboutpages", force: :cascade do |t|
     t.string   "title"
@@ -92,6 +92,12 @@ ActiveRecord::Schema.define(version: 20161130012936) do
     t.datetime "updated_at",  null: false
     t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
