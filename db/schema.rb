78ff06b0259b56f26385962b92620f2d00dcb843
melisserrr <mtsnider@gmail.com>
2016-11-29 19:10:33 -0600

    adde orderitem model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130003530) do
+ActiveRecord::Schema.define(version: 20161130010810) do
 
   create_table "aboutpages", force: :cascade do |t|
     t.string   "title"
@@ -81,6 +81,17 @@ ActiveRecord::Schema.define(version: 20161130003530) do
     t.integer  "order_id"
     t.index ["order_id"], name: "index_customers_on_order_id"
     t.index ["province_id"], name: "index_customers_on_province_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.decimal  "quantity"
+    t.decimal  "total_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
